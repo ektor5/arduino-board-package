5cb2bf921c2f0a21f5080748c3d0c66fff292554
ektor5 <ek5.chimenti@gmail.com>
2016-02-18 17:26:40 +0100

    Remove compilation object and linking files

--- udoo/solox/variants/udooneo/mqx/release/open-amp/dep/llist.d
+++
@@ -1,3 +0,0 @@
-/media/tcmichals/FastDisk/Neo/portOpenAmp/udooneo-dev/mqx/lib/imx6sx_sdb_m4.gcc_arm/release/open-amp/obj/llist.o: \
- /media/tcmichals/FastDisk/Neo/portOpenAmp/udooneo-dev/mqx/open-amp/common/llist/llist.c \
- /media/tcmichals/FastDisk/Neo/portOpenAmp/udooneo-dev/mqx/open-amp/common/llist/llist.h
--- udoo/solox/variants/udooneo/mqx/release/psp/dep/unicode.d
+++
@@ -1,9 +0,0 @@
-/home/udoodev/build/mqx/udooneo-dev/mqx/lib/imx6sx_sdb_m4.gcc_arm/release/psp/obj/unicode.o: \
- /home/udoodev/build/mqx/udooneo-dev/mqx/mqx/source/string/unicode.c \
- /usr/local/gcc-arm-none-eabi-4_9-2015q3//lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
- /usr/local/gcc-arm-none-eabi-4_9-2015q3//arm-none-eabi/include/stdint.h \
- /usr/local/gcc-arm-none-eabi-4_9-2015q3//arm-none-eabi/include/machine/_default_types.h \
- /usr/local/gcc-arm-none-eabi-4_9-2015q3//arm-none-eabi/include/sys/features.h \
- /usr/local/gcc-arm-none-eabi-4_9-2015q3//arm-none-eabi/include/sys/_intsup.h \
- /usr/local/gcc-arm-none-eabi-4_9-2015q3//arm-none-eabi/include/sys/_stdint.h \
- /home/udoodev/build/mqx/udooneo-dev/mqx/mqx/source/include/unicode.h
